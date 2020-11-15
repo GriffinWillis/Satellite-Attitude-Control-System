3411fa12e36985594712088fbc7f8fe3f7460902
Griffin Willis <gtwfg8@mail.missouri.edu>
2020-11-14 21:54:35 -0600

    Update Main_Program.ino

--- Software/Arduino Code/Main_Program.ino
+++ Software/Arduino Code/Main_Program.ino
@@ -79,13 +79,6 @@ double velocity; // stores velocity in rpm.
 double U; // Voltage, number 0−255, 255 results in a 24V of output voltage. 
 */
 
-// Code below is written by Jeff Rowberg and can be found at https://github.com/jrowberg/i2cdevlib 
-
-#include "I2Cdev.h" 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE 
-  #include "Wire.h" 
-#endif 
-
 bool blinkState = false;
 
 // ================================================================ 
@@ -93,15 +86,8 @@ bool blinkState = false;
 // ================================================================ 
 
 void setup() {
-// join I2C bus (I2Cdev library doesn't do this automatically) 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE 
   Wire.begin(); 
-  // TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
   Wire.setClock(400000); // 400kHz I2C clock. 
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE 
-  Fastwire::setup(400, true); 
-#endif 
-
 // initialize serial communication
 Serial.begin(9600);  // initialize serial bus (Serial Monitor) 
 while(!Serial); // wait for serial initialization
@@ -115,14 +101,6 @@ if(IMU.begin()){  // initialize IMU
   }
   Serial.println();
   filter.begin(SAMPLE_RATE);  // initialize Madgwick filter
-
-// NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio 
-// Pro Mini running at 3.3v, cannot handle this baud rate reliably due to 
-// the baud timing being too misaligned with processor ticks. You must use 
-// 38400 or slower in these cases, or use some kind of external separate 
-// crystal solution for the UART timer. 
- 
-// Code written by Jeff Rowberg ends here. 
 
 Time = micros(); // Return time since the arduino was powered on. 
 
@@ -142,7 +120,6 @@ PID_controller.SetOutputLimits(-255, 255);
 //state3 = 0; // Sets initial state to 0. 
 } 
 
-
 // ================================================================ 
 // === MAIN PROGRAM LOOP === 
 // ================================================================ 
@@ -155,8 +132,8 @@ void loop() {
     static unsigned long previousTime = millis();
     unsigned long currentTime = millis();
     if (currentTime - previousTime >= 1000/SAMPLE_RATE){
-      //printValues();
-      //printRotationAngles();
+      printValues();
+      printRotationAngles();
       previousTime = millis();
     }
 
@@ -169,64 +146,16 @@ void loop() {
     delay(1000);
   }
   
-  pitch_deg = filter.getPitch() - pitch_offset; // Stores data from the gyro in a varible, was 
-    // adjusted so the angle is 0 when the cube is in horizontal position. 
+  pitch_deg = filter.getPitch() - pitch_offset; // Stores data from the gyro in a variable, was 
+    // adjusted so the angle is 0 when the satellite body is in its initial position. 
   
-  input = pitch_deg; // Uses angle as input signal for the PID controller. 
-
-/* 
-  //Calculation of the angular speed of the reaction wheels using Hall effect sensors.
-  
-  //Read value from hall effect sensor 1 
-  Sensorval1 = analogRead(magnetSensor1); // 560 55 
-  
-  //Read value from hall effect sensor 2 
-  Sensorval2 = analogRead(magnetSensor2); // 650 630 
-
-      if(state3 == 1){ 
-      if(state1 == 1 && Sensorval1 > 500){ //If sensor 1 is sensing a magnet, used to calculate direction 
-        state2 = 1; 
-        state3 = 0; 
-        //Serial.println(state2);
-      } 
-    
-      if(state1 == 1 && Sensorval2 > 600){ //If sensor 2 is sensing a magnet, used to calculate direction 
-        state2 = 2; 
-        state3 = 0; 
-        //Serial.println(state2); 
-      }
-    } 
-
-    if(Sensorval1 > 500 && Sensorval2 > 600){ // If both sensor is sensing a magnet 
-      if (state1 == 1) { //If both sensors has been low, to avoid measure many times at the same magnet. 
-        currentTime = micros(); // get time 
-        dx = currentTime - oldTime; // calculate time difference 
-        radVelocity = 1000000*3.1415/dx; // calculate angular speed 
-        oldTime = currentTime; // set current time to new time 
-    
-        state1 = 0; 
-        state3 = 1;
-      }
-    } 
-
-    if(Sensorval1 < 500 && Sensorval2 < 600){ // If both sensors are low, change state to 1. 
-      state1 = 1; 
-    } 
-  
-  if(state2 == 1){ // State2 stores the direction that the wheel is spinning. 
-    velocity = -1*radVelocity; // Neagtive direction 
-  }
-  if(state2 == 2){
-    velocity = radVelocity; // Positive. 
-  } 
-*/
+  input = pitch_deg; // Uses angle as input signal to the PID controller. 
 
   // Code for PID 
-  setpoint = 45; // 0 degrees offset from the equillibrium 
+  setpoint = 45; // Desired orientation set manually by the user
   
     if(micros()-Time > 8000000){ // Needed a delay so that the IMU could stabilaze, using delay() caused overflow. 
       PID_controller.Compute(); // Compute new output value 
-      // U=k2*velocity+(R/k2)*output; // Compute voltage the gives a certain torque, depending on velocity speed. 
   
     // if((input>46) || (input<44)){
       if(output >= 0){ // Since arduino only works with positive current, the direction was swapped instead. 
@@ -240,15 +169,19 @@ void loop() {
     }
   } 
 
+// Output pwm signal. 
+analogWrite(pwmPin, pwmSignal); 
+
 //Serial.print(roll_deg); // Print the angle in serial monitor. 
 //Serial.print(" "); 
 //Serial.print(input); // Print input 
 //Serial.print(" "); 
 //Serial.println(output); // Print output 
-
-// Output pwm signal. 
-analogWrite(pwmPin, pwmSignal); 
 }
+
+// ================================================================ 
+// === FUNCTIONS === 
+// ================================================================ 
 
 // Prints IMU values.
 void printValues(){
@@ -259,12 +192,12 @@ void printValues(){
    // Retrieve and print IMU values
    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()
       && IMU.readAcceleration(ax, ay, az) && IMU.readGyroscope(gx, gy, gz)) {
-      Serial.print("ax = ");  Serial.print(dtostrf(ax, 4, 1, buffer));  Serial.print(" g, ");
-      Serial.print("ay = ");  Serial.print(dtostrf(ay, 4, 1, buffer));  Serial.print(" g, ");
-      Serial.print("az = ");  Serial.print(dtostrf(az, 4, 1, buffer));  Serial.print(" g, ");
-      Serial.print("gx = ");  Serial.print(dtostrf(gx, 7, 1, buffer));  Serial.print(" °/s, ");
+      //Serial.print("ax = ");  Serial.print(dtostrf(ax, 4, 1, buffer));  Serial.print(" g, ");
+      //Serial.print("ay = ");  Serial.print(dtostrf(ay, 4, 1, buffer));  Serial.print(" g, ");
+      //Serial.print("az = ");  Serial.print(dtostrf(az, 4, 1, buffer));  Serial.print(" g, ");
+      Serial.print("gx = ");  Serial.print(dtostrf(gx, 7, 1, buffer));  Serial.print(" °/s, ");   // gx or gy will be the rotation speed of satellite
       Serial.print("gy = ");  Serial.print(dtostrf(gy, 7, 1, buffer));  Serial.print(" °/s, ");
-      Serial.print("gz = ");  Serial.print(dtostrf(gz, 7, 1, buffer));  Serial.println(" °/s");
+      //Serial.print("gz = ");  Serial.print(dtostrf(gz, 7, 1, buffer));  Serial.println(" °/s");
    }
 }
 
@@ -283,9 +216,9 @@ void printRotationAngles(){
       filter.updateIMU(gx, gy, gz, ax, ay, az);  // update roll, pitch, and yaw values
 
       // Print rotation angles
-      Serial.print("Roll = ");  Serial.print(dtostrf(filter.getRoll(), 4, 0, buffer)); Serial.println(" °, ");
-      Serial.print("Pitch = ");  Serial.print(dtostrf(filter.getPitch(), 4, 0, buffer)); Serial.print(" °, ");
-      Serial.print("Yaw = ");  Serial.print(dtostrf(filter.getYaw(), 4, 0, buffer)); Serial.println(" °");
+      //Serial.print("Roll = ");  Serial.print(dtostrf(filter.getRoll(), 4, 0, buffer)); Serial.print(" °, ");
+      Serial.print("Pitch = ");  Serial.print(dtostrf(filter.getPitch(), 4, 0, buffer)); Serial.println(" °, ");
+      //Serial.print("Yaw = ");  Serial.print(dtostrf(filter.getYaw(), 4, 0, buffer)); Serial.print(" °");
    }
 }
 
